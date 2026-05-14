6639a9867e83d708c5a5f047f7d255ab69ec3334
Syed Gul Ahmed <syedgul.cs17@iba-suk.edu.pk>
2026-05-14 08:30:50 +0500

    Initial commit: Excel upload endpoint

---
+++ src/main/java/com/example/excel_chat/ExcelChatApplication.java
@@ -0,0 +1,13 @@
+package com.example.excel_chat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExcelChatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExcelChatApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/excel_chat/ExcelChatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.excel_chat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExcelChatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
